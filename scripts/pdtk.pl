d96c0c4cc6cc8e26f1c309b0cbc0a1edd8fbe602
Lee Katz - Aspen <gzu2@cdc.gov>
2023-09-01 14:05:22 -0400

    force cache

--- scripts/pdtk.pl
+++ scripts/pdtk.pl
@@ -62,7 +62,7 @@ sub main{
   if($$settings{query}){
     my $res = querySample($settings);
     my @sampleHit = sort keys(%$res);
-    print Dumper $res;
+    print Dumper {res=>$res};
 
     # Print the header from the db
     my @header = keys($$res{$sampleHit[0]});
